e979c6587993a93e2f4b03ca108b76a7fad0cc7c
willy <willybalt.2017@gmail.com>
2025-03-14 23:05:50 +0100

    se agrego responsividad y js al menu

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+const menutoggle = document.getElementById('menutoggle');
+const links = document.getElementById('links');
+
+// Abrir y cerrar el menú con el botón
+menutoggle.addEventListener('click', (event) => {
+    links.classList.toggle('active');
+    event.stopPropagation(); // Evitar que el clic en el botón se propague
+});
+
+// Cerrar el menú si se hace clic fuera de él
+document.addEventListener('click', (event) => {
+    // Si el clic no fue en el menú ni en el botón, cerramos el menú
+    if (!links.contains(event.target) && !menutoggle.contains(event.target)) {
+        links.classList.remove('active');
+    }
+});
